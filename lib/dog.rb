2cfe716a986471f00238209a949da1b12cdb2f9a
steve <stevemighty2030@gmail.com>
2022-10-21 13:17:23 +0300

    the lab is passed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  # setter method
+  def name=(dogs_name)
+    @this_dogs_name=dogs_name
+  end
+  # getter method
+  def name
+    @this_dogs_name
+  end
+
 
 end
+odie=Dog.new
+odie.name="odie"
+# call the dog/ name method
+puts odie.name
